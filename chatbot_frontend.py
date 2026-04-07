4394da8bbd5334cc5e8aaea5b7bdfc86eb515d9e
aishwaryakore <aishwaryakore07@gmail.com>
2026-04-06 23:49:50 -0400

    integrated langsmith for observability

--- chatbot_frontend.py
+++ chatbot_frontend.py
@@ -32,7 +32,14 @@ if 'chat_threads' not in st.session_state:
 
 add_thread(st.session_state['thread_id'])
 
-CONFIG = {'configurable' : {'thread_id' : st.session_state['thread_id']}}
+# CONFIG = {'configurable' : {'thread_id' : st.session_state['thread_id']}}
+CONFIG={
+    "configurable": {'thread_id' : st.session_state['thread_id']},
+    "metadata": {
+        "thread_id" : st.session_state['thread_id']
+    },
+    "run_name": "chat-turn"
+}
 
 st.sidebar.title("AI Chatbot")
 
