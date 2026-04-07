835d2b0e825ee9186c1d7ec0bb03e2075db74b34
aishwaryakore <aishwaryakore07@gmail.com>
2026-04-06 23:15:49 -0400

    integrated sqlite db for persistence

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -3,8 +3,10 @@ from langgraph.graph.message import add_messages
 from langgraph.checkpoint.memory import InMemorySaver
 from langchain_core.messages import BaseMessage
 from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.sqlite import SqliteSaver
 from dotenv import load_dotenv
 from typing import TypedDict, Annotated
+import sqlite3
 
 load_dotenv()
 
@@ -18,7 +20,9 @@ def chat_node(state : ChatbotState):
     response = llm.invoke(messages)
     return {'messages' : [response]}
 
-checkpointer = InMemorySaver()
+db_connection = sqlite3.connect(database='chatbot.db', check_same_thread=False)
+
+checkpointer = SqliteSaver(conn=db_connection)
 
 graph = StateGraph(ChatbotState)
 
@@ -28,3 +32,10 @@ graph.add_edge(START, 'chat_node')
 graph.add_edge('chat_node', END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
+
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+
+    return list(all_threads)
